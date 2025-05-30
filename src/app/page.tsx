02527fe4b9fed6c2b9d761c30b9c8867f5378da8
josueJURE <josue.jure@gmail.com>
2025-05-30 10:56:33 +0100

    (feat:js) grab country name on onMouseEnter, create onMouseLeave

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,21 @@ import {
   ZoomableGroup,
 } from "react-simple-maps";
 import { Switch } from "@/components/ui/switch";
-import ReactTooltip from "react-tooltip";
+import { Tooltip } from "react-tooltip";
 import React, { useState } from "react";
+
+const markers = [
+  {
+    markerOffset: -15,
+    name: "Sau Paulo",
+    coordinates: [-58.3016,-34.6037],
+  }
+]
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json";
 
 export default function Home() {
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState<string>("");
   return (
     <main className="min-h-screen w-full flex items-center justify-center p-4">
       <Card className="w-full max-w-xl p-6">
@@ -25,7 +33,9 @@ export default function Home() {
           Unsure what to cook? Let recipe for sucess inspire your next meal from
           any country in the world
         </h1>
+        <h1>{content}</h1>
         <p className="text-center text-gray-600 mb-4">Select a country</p>
+        <Tooltip id="country-tooltip" style={{ zIndex: 100 }}>{content}</Tooltip>
 
         <ComposableMap data-tip="">
           <ZoomableGroup zoom={1}>
@@ -34,6 +44,14 @@ export default function Home() {
               {({ geographies }) =>
                 geographies.map((geo) => (
                   <Geography
+                  onMouseEnter={()=> {
+                    const NAME = geo.properties.name;
+                    console.log("countries name;", geo.properties.name)
+                    setContent(NAME)
+                  } }
+                  onMouseLeave={() =>{
+                    setContent("")
+                  }}
                     key={geo.rsmKey}
                     geography={geo}
                     fill="#EAEAEC"
@@ -47,9 +65,17 @@ export default function Home() {
                 ))
               }
             </Geographies>
+            {/* {markers.map({name, coordinates, markerOffset})} */}
+     
           </ZoomableGroup>
         </ComposableMap>
       </Card>
     </main>
   );
 }
+
+
+
+
+
+   
