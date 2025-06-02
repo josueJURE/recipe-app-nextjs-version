3c37373c78afa3c78676cdae17987de10168009d
josueJURE <josue.jure@gmail.com>
2025-06-02 20:45:24 +0100

    (feat:JS) : add onChange event to input field

--- src/components/ui/fieldset.tsx
+++ src/components/ui/fieldset.tsx
@@ -14,10 +14,16 @@ const dietaryRequirements = [
 
 function Fieldset() {
   const [input, setInput] = useState<boolean>(false);
+  const [text, setText] = useState<string>("");
   function handleToggleOfDietaryRequirements() {
     return setInput((checked) => !checked);
   }
+  function handleUserInput(e: React.ChangeEvent<HTMLInputElement>): void {
+    setText(e.target.value);
+  }
   console.log(input);
+  console.log(text);
+
   return (
     <fieldset aria-label="allergies section" className="allergies">
       <legend>Indicate any dietary requirements</legend>
@@ -32,18 +38,20 @@ function Fieldset() {
                 }}
               />
               <Label htmlFor="terms">Other?</Label>
-              {input && <Input />}
+              {input && <Input onChange={handleUserInput} />}
             </div>
           ) : (
             <div className="flex items-center gap-3" key={index}>
               {" "}
               <Checkbox />
               <Label htmlFor="terms">lactose intolerant?</Label>
+           
             </div>
           )
         )}
     
       </div>
+    
     </fieldset>
   );
 }
