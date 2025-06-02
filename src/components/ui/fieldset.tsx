218d3fbac56bfd37f458059b40a59eaa64b5e355
josueJURE <josue.jure@gmail.com>
2025-06-02 10:03:01 +0100

    (feat:JS) : map over dietaryRequirements to create Checkboxes

--- src/components/ui/fieldset.tsx
+++ src/components/ui/fieldset.tsx
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "./input";
 import { Label } from "@/components/ui/label";
 
+const dietaryRequirements = [
+  {
+    question: "Vegan?",
+  },
+  {
+    question: "Other?",
+  },
+];
+
 function Fieldset() {
+  const [input, setInput] = useState<boolean>(false);
+  function handleToggleOfDietaryRequirements() {
+    return setInput((checked) => !checked);
+  }
+  console.log(input);
   return (
     <fieldset aria-label="allergies section" className="allergies">
-      <legend className="legend">Indicate any dietary requirements</legend>
+      <legend>Indicate any dietary requirements</legend>
       <div className="flex flex-row gap-6">
-        <div className="flex items-center gap-3">
-          {" "}
-          <Checkbox />
-          <Label htmlFor="terms">lactose intolerant?</Label>
-        </div>
-        <div className="flex items-center gap-3">
-          <Checkbox />
-          <Label htmlFor="terms">Vegan?</Label>
-        </div>
-        <div className="flex items-center gap-3">
-          <Checkbox />
-          <Label htmlFor="terms">Other?</Label>
-          <input/>
-        </div>
-
+        {dietaryRequirements.map((requirement, index) =>
+          requirement.question === "Other?" ? (
+            <div className="flex items-center gap-3">
+              <Checkbox
+                checked={input}
+                onCheckedChange={() => {
+                  handleToggleOfDietaryRequirements();
+                }}
+              />
+              <Label htmlFor="terms">Other?</Label>
+              {input && <Input />}
+            </div>
+          ) : (
+            <div className="flex items-center gap-3">
+              {" "}
+              <Checkbox />
+              <Label htmlFor="terms">lactose intolerant?</Label>
+            </div>
+          )
+        )}
     
       </div>
     </fieldset>
