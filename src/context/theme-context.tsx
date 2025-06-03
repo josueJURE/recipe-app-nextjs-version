3efcb870a527cca29244d1aa5bc1f320b43b456b
josueJURE <josue.jure@gmail.com>
2025-06-03 09:22:37 +0100

    (feat:JS) : create a dark mode context/provider

---
+++ src/context/theme-context.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+type ThemeContextType = {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    document.documentElement.classList.toggle("dark");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+
+export function useTheme() {
+    const context = useContext(ThemeContext);
+    if (context === undefined) {
+        throw new Error("useTheme must be used within a ThemeProvider")
+    }
+
+    return context
+}
