29933fff9430c9edb71a768fff266c9651d239f8
josueJURE <josue.jure@gmail.com>
2025-06-08 13:21:55 +0100

    (feat:JS) write better prompt for Open AI

--- src/app/api/updateRecipe/route.ts
+++ src/app/api/updateRecipe/route.ts
@@ -5,11 +5,11 @@ const openai = new OpenAI({
   apiKey: process.env.openaiAPI,
 });
 
-let recipe = "";
-for (let i = 0; i < 20; i++) { // Adjust loop count
-  recipe += "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ";
-}
-console.log(recipe);
+// let recipe = "";
+// for (let i = 0; i < 20; i++) { // Adjust loop count
+//   recipe += "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ";
+// }
+// console.log(recipe);
 
 
 
@@ -19,20 +19,37 @@ export async function POST(request: Request) {
     const  { countrySelected, dietaryRequirements } = await request.json();
     console.log("countrySelected:", countrySelected);
     console.log("dietaryRequirements:", dietaryRequirements);
-    
-    // const response = await openai.chat.completions.create({
-    //   messages: [
-    //     {
-    //       role: "user",
-    //       content: `Give me a traditional recipe from ${countrySelected}. Include ingredients and step-by-step instructions.`,
-    //     },
-    //   ],
-    //   model: "gpt-3.5-turbo",
-    // });
 
-    // const recipe = response.choices[0]?.message?.content || "No recipe found";
+    const vegan = "taking into account the fact that I'm vegan"
+
+    const otherText =
+    dietaryRequirements.other.checked && dietaryRequirements.other.text !== ""
+      ? dietaryRequirements.other.text
+      : "";
+
+       // Build the full prompt string
+    const prompt = `Give me a traditional recipe from ${countrySelected}. ${
+      dietaryRequirements.vegan ? vegan : ""
+    }. ${otherText}. Include ingredients and step-by-step instructions.`;
+
+    console.log("Prompt sent to OpenAI:", prompt);
+
+
+    
+    const response = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+
+    const recipe = response.choices[0]?.message?.content || "No recipe found";
+ 
     // const recipe = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati vero nihil dolores officia sequi dicta. Quo ratione ab adipisci culpa aliquam, facilis reprehenderit odio at sapiente inventore laborum ullam qui"
-    // console.log("Generated recipe:", recipe);
+    console.log("Generated recipe:", prompt);
     
     return NextResponse.json({recipe});
     
