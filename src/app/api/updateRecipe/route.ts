ca4c8b3725fc8b1346fed05f66424c9db68d36bb
josueJURE <josue.jure@gmail.com>
2025-06-08 09:54:52 +0100

    (feat:JS): comment out response form Open AI

--- src/app/api/updateRecipe/route.ts
+++ src/app/api/updateRecipe/route.ts
@@ -13,17 +13,18 @@ export async function POST(request: Request) {
     console.log("countrySelected:", countrySelected);
     console.log("dietaryRequirements:", dietaryRequirements);
     
-    const response = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: `Give me a traditional recipe from ${countrySelected}. Include ingredients and step-by-step instructions.`,
-        },
-      ],
-      model: "gpt-3.5-turbo",
-    });
+    // const response = await openai.chat.completions.create({
+    //   messages: [
+    //     {
+    //       role: "user",
+    //       content: `Give me a traditional recipe from ${countrySelected}. Include ingredients and step-by-step instructions.`,
+    //     },
+    //   ],
+    //   model: "gpt-3.5-turbo",
+    // });
 
-    const recipe = response.choices[0]?.message?.content || "No recipe found";
+    // const recipe = response.choices[0]?.message?.content || "No recipe found";
+    const recipe = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati vero nihil dolores officia sequi dicta. Quo ratione ab adipisci culpa aliquam, facilis reprehenderit odio at sapiente inventore laborum ullam qui"
     console.log("Generated recipe:", recipe);
     
     return NextResponse.json({ recipe });
