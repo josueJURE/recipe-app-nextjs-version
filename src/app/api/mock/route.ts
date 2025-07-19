0438f9ff261575fbd5c172683e91ec7a6f7b2903
josueJURE <josue.jure@gmail.com>
2025-07-19 19:34:34 +0100

    remove commented out code

--- src/app/api/mock/route.ts
+++ src/app/api/mock/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 import Welcome from "@/components/ui/Welcome/welcome";
-import { success } from "zod";
+
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
