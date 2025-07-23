e445b33819c962291bacb8ec7569d57fb4dba0ba
josueJURE <josue.jure@gmail.com>
2025-07-23 11:42:34 +0100

    (feat:eslint): fix react/no-unescaped-entities eslint error by using &apo

--- src/components/ui/Welcome/welcome.tsx
+++ src/components/ui/Welcome/welcome.tsx
@@ -6,7 +6,7 @@ import * as React from "react";
 export default function Welcome({recipe}: {recipe: string}) {
   return (
     <Html>
-          <h1>Here's Your Recipe!</h1>
+          <h1>Here&apo;s Your Recipe!</h1>
           <p>{recipe}</p>
       <Button
         href="https://example.com"
