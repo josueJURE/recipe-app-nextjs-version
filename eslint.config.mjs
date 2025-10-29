73dc069afd2e69e9ee6411c99f3f0c1f90704e97
josueJURE <josue.jure@gmail.com>
2025-10-29 12:37:52 +0000

    (feat:eslint): modify eslint.config.mjs file to fix error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,47 +9,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-export default [
+const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
     },
   },
-
-  // 🚫 Ignore Prisma-generated code
   {
-    ignores: [
-      "node_modules/**",
-      "prisma/**",
-      ".prisma/**",
-    ],
+    // Ignore generated Prisma client files
+    ignores: ["src/generated/**/*"],
   },
 ];
 
-
-
-
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-//   {
-//     rules: {
-//       "@typescript-eslint/no-unused-vars": "warn",
-//     },
-    
-//   },
-// ];
-
-// export default eslintConfig;
+export default eslintConfig;
