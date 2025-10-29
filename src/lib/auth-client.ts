d02e6fd731f8f04f126333708dbb26886f7f6b3f
josueJURE <josue.jure@gmail.com>
2025-10-29 14:17:34 +0000

    (feat:.chore): commit before switching branch

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,9 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.NODE_ENV === "development" 
+        ? "http://localhost:3000" 
+        : "https://recipe-app-nextjs-version.onrender.com"
 }) 
 
 
